0a7ce9daff7e41d4fcec43cf5259179caac15c01
joseph6377 <31067497+joseph6377@users.noreply.github.com>
2023-06-05 20:21:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ def save_workout_data(data):
     with open("workout_data.json", "w") as file:
         json.dump(data, file)
 
-def add_workout_entry(day, exercise, sets, reps, weight):
+def add_workout_entry(day, exercise, reps, weight):
     data = load_workout_data()
     entry = {
         "date": datetime.now().strftime("%Y-%m-%d"),
@@ -78,14 +78,7 @@ def add_workout_entry(day, exercise, sets, reps, weight):
         "sets": [],
     }
 
-    for set_num in range(1, sets + 1):
-        set_header = f"Set {set_num}"
-        with st.expander(set_header):
-            reps_input_key = f"reps-{exercise}-{set_num}"
-            weight_input_key = f"weight-{exercise}-{set_num}"
-            reps_input = st.number_input(f"Reps for Set {set_num}", value=reps, step=1, key=reps_input_key)
-            weight_input = st.number_input(f"Weight for Set {set_num} (in kg)", value=weight, step=0.5, key=weight_input_key)
-            entry["sets"].append({"reps": reps_input, "weight": weight_input})
+    entry["sets"].append({"reps": reps, "weight": weight})
 
     if day not in data:
         data[day] = []
@@ -93,31 +86,41 @@ def add_workout_entry(day, exercise, sets, reps, weight):
     save_workout_data(data)
     st.write("Workout entry added successfully.")
 
-
 def display_workout_entries():
     workout_entries = []
     data = load_workout_data()
+
     for day, entries in data.items():
         workout_entries.extend(entries)
 
     if not workout_entries:
         st.write("No workout entries found.")
     else:
-        st.write("Here are all your workout entries:")
+        workout_entries = sorted(workout_entries, key=lambda x: (x["date"], x["exercise"]), reverse=True)
+        grouped_entries = {}
+
         for entry in workout_entries:
             date = entry["date"]
             exercise = entry["exercise"]
-            sets = entry["sets"]
+            key = (date, exercise)
+            if key not in grouped_entries:
+                grouped_entries[key] = []
+            grouped_entries[key].append(entry)
 
-            st.write(f"Date: {date}")
+        st.write("Here are all your workout entries:")
+
+        for (date, exercise), entries in grouped_entries.items():
+            st.subheader(date)
             st.write(f"Exercise: {exercise}")
-            st.write(f"Number of Sets: {len(sets)}")
+            st.write(f"Number of Sets: {len(entries)}")
 
-            for set_num, set_data in enumerate(sets):
-                reps = set_data["reps"]
-                weight = set_data["weight"]
-                st.write(f"Set {set_num + 1}: Reps={reps}, Weight={weight} kg")
+            for entry in entries:
+                set_data = entry["sets"][0]
+                reps = set_data.get("reps", 0)
+                weight = set_data.get("weight", 0)
+                st.write(f"Set: Reps={reps}, Weight={weight} kg")
 
+            st.write("---")
 
 def main():
     st.title("Workout Tracker")
@@ -127,21 +130,14 @@ def main():
     if choice == "Add a workout entry":
         day = st.selectbox("Select a combo", list(exercise_data.keys()))
         exercise = st.selectbox("Select an exercise", exercise_data[day])
-        sets = st.number_input("Enter the number of sets", value=0, step=1)
-
-        if sets > 0:
-            st.write("Enter the reps and weight for each set:")
-            for set_num in range(1, sets + 1):
-                st.write(f"Set {set_num}")
-                reps = st.number_input("Reps", value=0, step=1, key=f"{day}-{exercise}-reps-{set_num}")
-                weight = st.number_input("Weight (in kg)", value=0.0, step=0.5, key=f"{day}-{exercise}-weight-{set_num}")
+        reps = st.number_input("Reps", value=0, step=1)
+        weight = st.number_input("Weight (in kg)", value=0.0, step=0.5)
 
         if st.button("Add Entry"):
-            add_workout_entry(day, exercise, sets, reps, weight)
+            add_workout_entry(day, exercise, reps, weight)
 
     elif choice == "Display all workout entries":
         display_workout_entries()
-
 
 if __name__ == "__main__":
     main()
